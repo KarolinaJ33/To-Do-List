45db01ec3d5a277cdff16378a59dc9531ab525df
Karolina Jasiówka <jasiowkakarolina@gmail.com>
2022-11-30 10:42:23 +0100

    added new task

--- js/script.js
+++ js/script.js
@@ -53,3 +53,25 @@
     
         bindEvents();
       };
+      const onFormSubmit = (event) => {
+        event.preventDefault();
+    
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        const newTaskInput = document.querySelector(".js-newTask");
+    
+        if (newTaskContent !== "") {
+          addNewTask(newTaskContent);
+          newTaskInput.value = "";
+        }
+        newTaskInput.focus();
+      };
+    
+      const init = () => {
+        render();
+    
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", onFormSubmit);
+      };
+    
+      init();
+    }
