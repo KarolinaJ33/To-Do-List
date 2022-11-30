ed89f329b73f4100c2942c70a20d061482c5d5d8
Karolina Jasiówka <jasiowkakarolina@gmail.com>
2022-11-30 10:24:56 +0100

    binding events

--- js/script.js
+++ js/script.js
@@ -11,3 +11,45 @@
 
         render();
     };
+    const bindEvents = () => {
+        const removeButtons = document.querySelectorAll(".js-remove");
+    
+        removeButtons.forEach((removeButton, index) => {
+          removeButton.addEventListener("click", () => {
+            removeTask(index);
+          });
+        });
+    
+        
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+    
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+          toggleDoneButton.addEventListener("click", () => {
+            toggleTaskDone(index);
+          });
+        });
+      };
+    
+      const render = () => {
+        let htmlString = "";
+    
+        for (const task of tasks) {
+          htmlString += `
+          <li class="list__item js-tasks">
+          <button class="list__button list__button--toggleDone js-done">
+              ${task.done ? "✓" : ""}
+          </button>
+          <span class="tasks__content${task.done ? " list__content--checked" : ""}">
+              ${task.content}
+          </span>
+          <button class="list__button list__button--remove js-remove">
+              🗑
+          </button>
+      </li>
+                      `;
+        }
+    
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    
+        bindEvents();
+      };
