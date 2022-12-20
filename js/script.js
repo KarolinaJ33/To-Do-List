0497313021dbd119c95e5d979ee5593b1f940f87
Karolina Jasiówka <jasiowkakarolina@gmail.com>
2022-12-20 11:36:26 +0100

    changed to immutability

--- js/script.js
+++ js/script.js
@@ -2,18 +2,30 @@
     const tasks = [];
 
     const addNewTask = (newTaskContent) => {
-        tasks.push({ content: newTaskContent, });
+      tasks = [
+        ...tasks,
+        { content: newTaskContent }
+      ];
 
         render();
     };
-    const removeTask = (index) => {
-        tasks.splice(index, 1);
 
+    const removeTask = (index) => {
+      tasks = [
+        ...tasks.slice(0, taskIndex),
+        ...tasks.slice(taskIndex + 1)
+      ];
+        
         render();
     };
 
     const toggleTaskDone = (taskIndex) => {
-        tasks[taskIndex].done = !tasks[taskIndex].done;
+      tasks = [ 
+        ...tasks.slice(0, taskIndex),
+        {...tasks[taskIndex].done = !tasks[taskIndex].done},
+        ...tasks.slice(taskIndex + 1)
+      ];
+        
         render();
     };
 
